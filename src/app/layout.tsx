91c0556ca4efc1d82159f6b7d9b7c9c4e77addb6
prechy123 <bamideleprecious100@gmail.com>
2024-09-18 10:24:36 +0100

    added favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,9 @@ import ToastProvider from "../providers/ToastProvider";
 export const metadata: Metadata = {
   title: "Dirchy",
   description:
-    "An All-In-One application to perform various operation to your various file format",
+    "An All-In-One application to perform various operation to your various file formats",
+  keywords:
+    "file converter, image resizer, video downloader, background remover, all-in-one app",
 };
 
 export default function RootLayout({
@@ -17,6 +19,26 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+      </head>
       <body
         className={` bg-[#ac8968] text-black dark:bg-[#3e362e] dark:text-white`}
         suppressHydrationWarning
