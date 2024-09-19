9eaf2eadb7f1470edaaaf147ac75eee4c1373499
prechy123 <bamideleprecious100@gmail.com>
2024-09-19 21:05:02 +0100

    update

--- src/components/tool/SingleInputTool.tsx
+++ src/components/tool/SingleInputTool.tsx
@@ -9,7 +9,7 @@ import useTextInput from "@/hooks/useTextInput";
 import useServerHandler from "@/hooks/useServerHandler";
 import Image from "next/image";
 
-const SingleInputTool = ({ tool }: { tool }) => {
+const SingleInputTool = ({ tool }) => {
   const toolName: string = decodeURIComponent(tool);
   const currentTool = tools.find((tool) => tool.name === toolName);
 
