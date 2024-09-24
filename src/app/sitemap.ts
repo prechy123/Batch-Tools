f5cc9e4424322deafed2d619f8c2588d90029cd4
prechy123 <bamideleprecious100@gmail.com>
2024-09-24 21:34:01 +0100

    feat/sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,21 @@
+import { MetadataRoute } from "next";
+import { tools } from "./tools";
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const toolEntries: MetadataRoute.Sitemap = tools.map(({ name }) => ({
+    url: `https://batchtools.site/${name}`,
+    lastModified: new Date(),
+    changeFrequency: "daily",
+    priority: 1,
+  }));
+
+  return [
+    {
+      url: "https://batchtools.site/",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.8,
+    },
+    ...toolEntries,
+  ];
+}
