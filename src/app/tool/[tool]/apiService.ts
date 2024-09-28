7da999e21201b338b4c4b841960d4e1e2fd8d56d
prechy123 <bamideleprecious100@gmail.com>
2024-09-28 16:00:03 +0100

    update

---
+++ src/app/tool/[tool]/apiService.ts
@@ -0,0 +1,402 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { showToast } from "@/utils/ShowToast";
+
+export async function handlePdfToWord(
+  file: any,
+  currentTool: any,
+  setDownloadLink: any,
+  setLoading: any
+) {
+  const reader: any = new FileReader();
+  reader.readAsDataURL(file);
+  reader.onload = async () => {
+    const base64FileData = reader?.result?.split(",")[1]; // get the base64 part without the data URI prefix
+    const fileName = file.name;
+
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}${currentTool?.backendPath}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          file_name: fileName,
+          file_data: base64FileData, // the base64-encoded file content
+        }),
+      }
+    );
+    if (response.ok) {
+      // Handle file download
+      const blob = await response.blob();
+      const url = URL.createObjectURL(blob);
+      setDownloadLink(url);
+      showToast("success", "Successfully converted, Press download button");
+      setLoading(false);
+    } else {
+      setLoading(false);
+      // const errorData = await response.json();
+      showToast("error", "Something went wrong, try again later");
+    }
+  };
+}
+
+export async function handleVideoToAudio(
+  file: any,
+  currentTool: any,
+  setDownloadLink: any,
+  setLoading: any
+) {
+  const formData = new FormData();
+  formData.append("video_file", file);
+
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_BACKEND_URL}${currentTool?.backendPath}`,
+    {
+      method: "POST",
+      body: formData,
+    }
+  );
+
+  if (response.ok) {
+    // Handle success, set the download link for the audio file
+    const blob = await response.blob();
+    const url = URL.createObjectURL(blob);
+    setDownloadLink(url);
+    showToast("success", "Successfully converted, Press download button");
+    setLoading(false);
+  } else {
+    const errorText = await response.text();
+    console.error("Error converting video to audio:", errorText);
+    showToast("error", "Something went wrong, try again later");
+    setLoading(false);
+  }
+}
+
+export async function handleImageResizer(
+  file: any,
+  width: any,
+  height: any,
+  currentTool: any,
+  setDownloadLink: any,
+  setImageLink: any,
+  setLoading: any
+) {
+  const formData = new FormData();
+  formData.append("image_file", file);
+  formData.append("width", width);
+  formData.append("height", height);
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_BACKEND_URL}${currentTool?.backendPath}`,
+    {
+      method: "POST",
+      body: formData,
+    }
+  );
+
+  if (response.ok) {
+    const blob = await response.blob();
+    const url = URL.createObjectURL(blob);
+    setDownloadLink(url);
+    setImageLink(url);
+    showToast("success", "Successfully resized, Press download button");
+    setLoading(false);
+  } else {
+    showToast("error", "Something went wrong, try again later");
+    setLoading(false);
+  }
+}
+
+export async function handleBackgroundRemover(
+  file: any,
+  currentTool: any,
+  setDownloadLink: any,
+  setImageLink: any,
+  setLoading: any
+) {
+  const formData = new FormData();
+  formData.append("image_file", file);
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_BACKEND_URL}${currentTool?.backendPath}`,
+    {
+      method: "POST",
+      body: formData,
+    }
+  );
+
+  if (response.ok) {
+    const blob = await response.blob();
+    const url = URL.createObjectURL(blob);
+    setDownloadLink(url);
+    setImageLink(url);
+    showToast(
+      "success",
+      "Successfully removed background, Press download button"
+    );
+    setLoading(false);
+  } else {
+    showToast("error", "Something went wrong, try again later");
+    setLoading(false);
+  }
+}
+
+export async function handleQRCodeGenerator(
+  qrUrl: any,
+  currentTool: any,
+  setDownloadLink: any,
+  setImageLink: any,
+  setLoading: any
+) {
+  const formData = new FormData();
+  formData.append("data", qrUrl);
+
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_BACKEND_URL}${currentTool?.backendPath}`,
+    {
+      method: "POST",
+      body: formData,
+    }
+  );
+
+  if (response.ok) {
+    const blob = await response.blob();
+    const url = URL.createObjectURL(blob);
+    setDownloadLink(url);
+    setImageLink(url);
+    showToast(
+      "success",
+      "Successfully generated qr code, Press download button"
+    );
+    setLoading(false);
+  } else {
+    const errorText = await response.text();
+    console.error("Error generating QR code:", errorText);
+    showToast("error", "Something went wrong, try again later");
+    setLoading(false);
+  }
+}
+
+export async function handleVideoTranscriber(
+  file: any,
+  currentTool: any,
+  setDownloadLink: any,
+  setLoading: any
+) {
+  const formData = new FormData();
+  formData.append("video_file", file);
+
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_BACKEND_URL}${currentTool?.backendPath}`,
+    {
+      method: "POST",
+      body: formData,
+    }
+  );
+
+  if (response.ok) {
+    const blob = await response.blob();
+    const url = URL.createObjectURL(blob);
+    setDownloadLink(url);
+    showToast("success", "Successfully transcribed, Press download button");
+    setLoading(false);
+  } else {
+    const errorText = await response.text();
+    console.error("Error transcribing video:", errorText);
+    showToast("error", "Something went wrong, try again later");
+    setLoading(false);
+  }
+}
+
+export async function handleYoutubeDownloader(
+  videoUrl: any,
+  currentTool: any,
+  setDownloadLink: any,
+  setLoading: any
+) {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_BACKEND_URL}${currentTool?.backendPath}`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ video_url: videoUrl }),
+    }
+  );
+  if (response.ok) {
+    const blob = await response.blob();
+    const url = window.URL.createObjectURL(
+      new Blob([blob], { type: "video/mp4" })
+    );
+    setDownloadLink(url);
+    showToast("success", "Successfully converted, Press download button");
+    setLoading(false);
+  } else {
+    const errorText = await response.text();
+    console.error("Error transcribing video:", errorText);
+    showToast("error", "Something went wrong, try again later");
+    setLoading(false);
+  }
+}
+
+export async function handleMovToMp4Converter(
+  file: any,
+  currentTool: any,
+  setDownloadLink: any,
+  setLoading: any
+) {
+  const formData = new FormData();
+  formData.append("video_file", file);
+
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_BACKEND_URL}${currentTool?.backendPath}`,
+    {
+      method: "POST",
+      body: formData,
+    }
+  );
+  if (response.ok) {
+    // Handle file download
+    const blob = await response.blob();
+    const url = URL.createObjectURL(blob);
+    setDownloadLink(url);
+    showToast("success", "Successfully converted, Press download button");
+    setLoading(false);
+  } else {
+    setLoading(false);
+    // const errorData = await response.json();
+    showToast("error", "Something went wrong, try again later");
+  }
+}
+
+export async function handlePdfMerger(
+  files: any,
+  currentTool: any,
+  setDownloadLink: any,
+  setLoading: any
+) {
+  const formData = new FormData();
+  for (let i = 0; i < files.length; i++) {
+    formData.append("pdf_files", files[i]);
+  }
+
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_BACKEND_URL}${currentTool?.backendPath}`,
+    {
+      method: "POST",
+      body: formData,
+    }
+  );
+  if (response.ok) {
+    // Handle file download
+    const blob = await response.blob();
+    const url = URL.createObjectURL(blob);
+    setDownloadLink(url);
+    showToast("success", "Successfully merged, Press download button");
+    setLoading(false);
+  } else {
+    setLoading(false);
+    // const errorData = await response.json();
+    showToast("error", "Something went wrong, try again later");
+  }
+}
+
+export async function handlePdfToJpegConverter(
+  file: any,
+  currentTool: any,
+  setImages: any,
+  setLoading: any
+) {
+  const reader = new FileReader();
+  reader.readAsDataURL(file);
+  reader.onload = async () => {
+    let base64FileData = "";
+    if (typeof reader?.result === "string") {
+      base64FileData = reader.result.split(",")[1];
+    }
+    const fileName = file.name;
+
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}${currentTool?.backendPath}`,
+      {
+        method: "POST",
+        body: JSON.stringify({
+          file_name: fileName,
+          file_data: base64FileData,
+        }),
+      }
+    );
+
+    if (response.ok) {
+      const data = await response.json();
+      setImages(data.images);
+      showToast("success", "Successfully converted, Press download button");
+      setLoading(false);
+    } else {
+      const errorText = await response.text();
+      console.error("Error transcribing video:", errorText);
+      showToast("error", "Something went wrong, try again later");
+      setLoading(false);
+    }
+  };
+}
+
+export async function handleHtmlToPdfConverter(
+  url: any,
+  currentTool: any,
+  setDownloadLink: any,
+  setLoading: any
+) {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_BACKEND_URL}${currentTool?.backendPath}`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ url }),
+    }
+  );
+
+  if (response.ok) {
+    const blob = await response.blob();
+    const pdfUrl = window.URL.createObjectURL(blob);
+    setDownloadLink(pdfUrl);
+    setLoading(false);
+    showToast("success", "Successfully converted, Press download button")
+  } else {
+    // const errorData = await response.json();
+    showToast("error", "Something went wrong, try again later");
+    setLoading(false);
+  }
+}
+
+export async function handleJsonToCsvConverter(
+  text: any,
+  currentTool: any,
+  setDownloadLink: any,
+  setLoading: any
+) {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_BACKEND_URL}${currentTool?.backendPath}`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: text,
+    }
+  );
+
+  if (response.ok) {
+    const blob = await response.blob();
+    const csvUrl = window.URL.createObjectURL(blob);
+    setDownloadLink(csvUrl);
+    setLoading(false);
+    showToast("success", "Successfully converted, Press download button")
+  } else {
+    // const errorData = await response.json();
+    showToast("error", "Invalid JSON format. Expected a list of dictionaries");
+    setLoading(false);
+  }
+}
---
+++ src/app/tool/[tool]/layout.tsx
@@ -0,0 +1,64 @@
+import { tools } from "@/app/tools";
+import { Metadata } from "next";
+
+interface Params {
+  params: {
+    tool: string;
+  };
+}
+
+export async function generateMetadata({ params }: Params): Promise<Metadata> {
+  const { tool } = params;
+
+  const currentTool = tools.find((t) => t.name === tool);
+
+  return {
+    title: `Batch Tools - ${tool.replace(/-/g, " ")}`,
+    description: currentTool?.description,
+    keywords: `${tool}, batch tools, Convert document format, background removal, qr code generator, video transcriber, pdf merger, html to pdf, pdf to jpg, json to csv`,
+    openGraph: {
+      title: `Batch Tools - ${tool.replace(/-/g, " ")}`,
+      description: currentTool?.description,
+      url: `https://batchtools.site/tool/${tool}`,
+      type: "website",
+      siteName: "Batch Tools",
+      images: [
+        {
+          url: "https://i.ibb.co/QYRccnw/Screenshot-2024-09-28-133605.png",
+          width: 1200,
+          height: 630,
+          alt: "Batch Tools Home Page",
+        },
+      ],
+      locale: "en_US",
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: `Batch Tools - ${tool.replace(/-/g, " ")}`,
+      description: currentTool?.description,
+      images: ["https://i.ibb.co/QYRccnw/Screenshot-2024-09-28-133605.png"],
+    },
+    robots: {
+      index: true,
+      follow: false,
+      googleBot: {
+        index: true,
+        follow: false,
+        noimageindex: true,
+        "max-snippet": -1,
+        "max-image-preview": "large",
+        "max-video-preview": -1,
+      },
+    },
+  };
+}
+
+function ToolLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return <>{children}</>;
+}
+
+export default ToolLayout;
